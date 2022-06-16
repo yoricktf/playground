9551fd69496df3c3aa8c554cfca9d35f326eef02
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-06-16 14:59:43 +0200

    routing login to the dashboard, changed the user model to add sitter details to increase user trust

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ const goToNewBooking = e => {
     <div>
     <h1>dashboard</h1>
     <ol>
-        <li>show details of the user</li>
+
         <li>SITTER show recent jobs and give something to search ore jobs</li>
         <li>PARENT a button to create a booking</li>
         <li>PARENT/SITTER show old jobs/bookings</li>
